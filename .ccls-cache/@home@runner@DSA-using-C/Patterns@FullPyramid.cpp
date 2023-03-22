4dcfa24e4a32fbfadc4b0d84f71b77ee22a07fc2
Vikas Chaudhary <88136062+vikasch1@users.noreply.github.com>
2023-03-22 07:31:07 +0000

    added condition for hollowpyramid

--- .ccls-cache/@home@runner@DSA-using-C/Patterns@FullPyramid.cpp
+++ .ccls-cache/@home@runner@DSA-using-C/Patterns@FullPyramid.cpp
@@ -11,7 +11,13 @@ int n;
       if(j<n-i-1){
         cout<<" ";
       }else if(k<2*i+1){
-        cout<<"*";
+        if(k==0 ||k==2*i ||i==n-1){
+            cout<<"*";
+          
+        }else{
+          cout<<" ";
+        }
+      
         k++;
         
       }else{
